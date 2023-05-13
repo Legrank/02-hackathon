2d9cc93fd23c92084f2c1c23f20246c1dd131ae7
JacobWestfield <zzultekk@gmail.com>
2023-05-13 20:12:08 +0300

    Reusable pickColor + new func in participantPage
    Сделал pickColor.js переиспользуемым, добавил конфиг в компонент страницы участника для выбора цвета в зависимости от технологии чтобы выполнялось задание хакатона

--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { pickColor } from "../utils/pickColor";
 
 const ProgressBar = ({ value, color, name }) => {
     return (
         <div className="progress" style={{ height: "30px" }}>
             <div
-                className={`progress-bar progress-bar-striped progress-bar-animated${color}`}
+                className={`progress-bar progress-bar-striped progress-bar-animated${pickColor(
+                    color
+                )}`}
                 role="progressbar"
                 aria-valuenow={value}
                 aria-valuemin="0"
--- src/utils/pickColor.js
+++ src/utils/pickColor.js
@@ -1,18 +1,20 @@
 /**
- * Returns className color for progress bar accroding to technology
- * @param {string} technology name of technology (HTML, CSS...)
+ * Returns className color
+ * @param {string} color  (red, green, blue, yellow, cyan)
  * @returns string className
  */
-export const pickColor = (technology) => {
-    switch (technology) {
-        case "html":
+export const pickColor = (color) => {
+    switch (color) {
+        case "green":
             return " bg-success";
-        case "react":
+        case "cyan":
             return " bg-info";
-        case "css":
+        case "yellow":
             return " bg-warning";
-        case "javaScript":
+        case "red":
             return " bg-danger";
+        case "blue":
+            return " bg-primary";
         default:
             return "";
     }
