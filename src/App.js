f817224f685663db83c5a79e27dd27d8cd18e242
JacobWestfield <zzultekk@gmail.com>
2023-05-13 21:30:11 +0300

    Redux added
    Добавил функционал редакса, добавил провайдер в index.js, добавил единоразовую загрузку в App.js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch, Redirect, useLocation } from "react-router-dom";
 import "./App.css";
 import NotFound from "./layouts/notFound";
@@ -8,9 +8,17 @@ import Test1 from "./layouts/test1";
 import Breadcrumbs from "./components/breadcrumbs";
 import Test2 from "./layouts/test2";
 import Favorites from "./layouts/favorites";
+import { useDispatch } from "react-redux";
+import { loadUsersList } from "./redux/users";
 
 function App() {
+    const dispatch = useDispatch();
     const location = useLocation();
+
+    useEffect(() => {
+        dispatch(loadUsersList());
+    }, []);
+
     return (
         <>
             <NavBar />
